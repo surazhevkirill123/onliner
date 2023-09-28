2322479e060ca73f3e9a4d4207fad9e5a5a1a4c1
Kirill Surazhev <surazhevkirillleonidovich@gmail.com>
2023-09-28 16:06:00 +0300

    minimal framework and first test

--- src/test/java/utils/DriverUtils.java
+++ src/test/java/utils/DriverUtils.java
@@ -6,18 +6,20 @@ import com.codeborne.selenide.WebDriverRunner;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.manager.SeleniumManager;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.util.Collections;
 
 public class DriverUtils {
     public static void openBrowser(){
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
-//        options.setExperimentalOption(
-//                "excludeSwitches", Collections.singletonList("enable-automation"));
-        WebDriverRunner.setWebDriver(new ChromeDriver(options));
+//        SeleniumManager.getInstance();
+//        WebDriverManager.chromedriver().setup();
+//        ChromeOptions options = new ChromeOptions();
+////        options.addArguments("--headless");
+////        options.setExperimentalOption(
+////                "excludeSwitches", Collections.singletonList("enable-automation"));
+//        WebDriverRunner.setWebDriver(new ChromeDriver(options));
         Selenide.open("https://google.com");
     }
 
